3726e8cef52559dbfa6324866daec9ba63d0836a
hdy86 <dyhong86@naver.com>
2023-02-08 17:24:39 +0900

    start project

---
+++ App.js
@@ -0,0 +1,28 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+
+function App() {
+  return (
+    <SafeAreaView>
+      <ScrollView contentInsetAdjustmentBehavior="automatic">
+        <Text>Test</Text>
+      </ScrollView>
+    </SafeAreaView>
+  );
+}
+
+export default App;

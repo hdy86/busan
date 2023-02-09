550e9c2fd643b166c27353344b4b7e24c88fa56a
hdy86 <dyhong86@naver.com>
2023-02-09 18:17:18 +0900

    home page

--- app/Screens/Home/index.js
+++ app/Screens/Home/index.js
@@ -1,17 +1,50 @@
 import React, {useEffect} from 'react';
-import {Text, View, Button} from 'react-native';
+import {
+  StyleSheet,
+  View,
+  Text,
+  Button,
+  Image,
+  ImageBackground,
+} from 'react-native';
 
 export default function Home({navigation}) {
   useEffect(() => {
     // console.log(navigation);
   }, []);
   return (
-    <View>
-      <Text>Home</Text>
-      <Button
-        title="Go to Alarm"
-        onPress={() => navigation.navigate('Alarm')}
-      />
+    <View style={styles.wrap}>
+      <ImageBackground
+        source={require('../../images/bg.jpg')}
+        resizeMode="cover"
+        style={styles.bg}>
+        <View style={styles.container}>
+          <View>
+            <Button title="한국어" />
+            <Button title="ENG" />
+          </View>
+          <View>
+            <Image source={require('../../images/logo.png')} />
+          </View>
+          <Text>Home</Text>
+          <Button
+            title="Go to Alarm"
+            onPress={() => navigation.navigate('Alarm')}
+          />
+        </View>
+      </ImageBackground>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  wrap: {
+    flex: 1,
+  },
+  container: {
+    paddingHorizontal: 20,
+  },
+  bg: {
+    flex: 1,
+  },
+});
